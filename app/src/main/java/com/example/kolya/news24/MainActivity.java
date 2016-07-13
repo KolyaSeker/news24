416a1ff73eb9fc0e3255b8673441ed7745b8e5e3
Kolya Sekerzhinskiy <kolyaseker@gmail.com>
2016-07-13 15:21:10 +0300

    NavigationView second commit

--- app/src/main/java/com/example/kolya/news24/MainActivity.java
+++ app/src/main/java/com/example/kolya/news24/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.kolya.news24;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.kolya.news24.Adapter.RecyclerViewAdapter;
 import com.example.kolya.news24.SimpleParse.NewsItem;
@@ -25,7 +35,8 @@ import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
     List<NewsItem> newsItemList;
     NewsResponse newsResponse;
     Realm realm;
@@ -33,17 +44,29 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     private SwipeRefreshLayout swipeRefreshLayout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initNavigationDrawer();
 
+        if (isOnline() == false) {
+
+            Context ctx = getBaseContext();
+            Toast.makeText(ctx, "No Internet connection!", Toast.LENGTH_SHORT).show();
+
+        }
         recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
 
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
+
+
 
         new FeedTask().execute();
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -66,18 +89,72 @@ public class MainActivity extends AppCompatActivity {
         realm.beginTransaction();
         realm.copyToRealm(newsItemList);
         realm.commitTransaction();
+
+
+
+
+    }
+
+    public void initNavigationDrawer() {
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id){
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.navigation_sub_item_1:
+                        Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.navigation_sub_item_2:
+                        Toast.makeText(getApplicationContext(),"Trash",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_exit:
+                        finish();
+
+                }
+                return true;
+            }
+        });
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
+
 
     public class FeedTask extends AsyncTask<Void, Void, List<NewsItem>> {
 
         @Override
         protected List<NewsItem> doInBackground(Void... params) {
             try {
+
 
                 //Creating OKHTTP Client
                 OkHttpClient client = new OkHttpClient();
@@ -119,6 +196,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public boolean isOnline() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo nInfo = connectivityManager.getActiveNetworkInfo();
+        if (nInfo != null && nInfo.isConnected()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
 
 
 }
