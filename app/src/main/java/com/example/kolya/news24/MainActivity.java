acfa9733b1a2c888336a943e270543e6db0c120f
Kolya Sekerzhinskiy <kolyaseker@gmail.com>
2016-07-15 11:53:40 +0300

    With realm cache

--- app/src/main/java/com/example/kolya/news24/MainActivity.java
+++ app/src/main/java/com/example/kolya/news24/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.kolya.news24;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -37,62 +39,107 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
-    List<NewsItem> newsItemList;
-    NewsResponse newsResponse;
-    Realm realm;
-
-    RecyclerView recyclerView;
+    public List<NewsItem> newsItemList;
+    public NewsResponse newsResponse;
+    public Realm realm;
+    public List<NewsItem> cachedNewsItem;
+    private RecyclerView recyclerView;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private boolean newsLoaded = false;
+    private boolean newsSaved = false;
+    private Handler saveHandler;
 
+
+    private Runnable trySaveNews = new Runnable() {
+        @Override
+        public void run() {
+            if (realm == null) {
+                realm = Realm.getDefaultInstance();
+            }
+
+            if(newsLoaded) {
+                realm.beginTransaction();
+                realm.copyToRealm(newsItemList);
+                realm.commitTransaction();
+             //   realm.close();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        saveHandler = new Handler();
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initNavigationDrawer();
-
-        if (isOnline() == false) {
-
-            Context ctx = getBaseContext();
-            Toast.makeText(ctx, "No Internet connection!", Toast.LENGTH_SHORT).show();
-
-        }
         recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
 
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
 
-
-
-        new FeedTask().execute();
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                new FeedTask().execute();
-            }
-        });
-
-
         swipeRefreshLayout.setColorSchemeResources(
                 android.R.color.holo_red_light);
 
-        swipeRefreshLayout.setRefreshing(false);
 
-        if (realm == null) {
-            realm = Realm.getDefaultInstance();
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener()
+        {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
+            {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState)
+            {
+                int firstPos = linearLayoutManager.findFirstCompletelyVisibleItemPosition();
+                if (firstPos>0)
+                {
+                    swipeRefreshLayout.setEnabled(false);
+                }
+                else {
+                    swipeRefreshLayout.setEnabled(true);
+                }
+            }
+        });
+
+        if (isOnline()) {
+
+            new FeedTask().execute();
+            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    new FeedTask().execute();
+                }
+            });
+
+
+
+            swipeRefreshLayout.setRefreshing(false);
+
+            if(!newsSaved) {
+                saveHandler.postDelayed(trySaveNews, 60000);
+                newsSaved = !newsSaved;
+            }
+            else {
+                saveHandler.removeCallbacks(trySaveNews);
+            }
         }
-        realm = Realm.getDefaultInstance();
-        realm.beginTransaction();
-        realm.copyToRealm(newsItemList);
-        realm.commitTransaction();
-
-
-
-
+        else {
+                if (realm == null) {
+                    realm = Realm.getDefaultInstance();
+                }
+                cachedNewsItem = realm.where(NewsItem.class).findAll();
+                RecyclerViewAdapter adapter = new RecyclerViewAdapter(cachedNewsItem, getApplicationContext());
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+        }
     }
 
     public void initNavigationDrawer() {
@@ -101,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-
+        Intent intent;
                 int id = menuItem.getItemId();
 
                 switch (id){
@@ -109,16 +156,34 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawers();
                         break;
-                    case R.id.navigation_sub_item_1:
-                        Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                    case R.id.navigation_sub_item_3:
+                        if (realm == null) {
+                            realm = Realm.getDefaultInstance();
+                        }
+                        realm.beginTransaction();
+                        realm.where(NewsItem.class).findAll().deleteAllFromRealm();
+                        realm.commitTransaction();
+
+
+                        recyclerView.removeAllViews();
+
+                        Toast.makeText(getApplicationContext(),"Clear a cache",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+
                         break;
+
                     case R.id.navigation_sub_item_2:
-                        Toast.makeText(getApplicationContext(),"Trash",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+
+                        intent = new Intent(MainActivity.this, InformationActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
                         drawerLayout.closeDrawers();
                         break;
+
                     case R.id.nav_exit:
                         finish();
-
                 }
                 return true;
             }
@@ -155,24 +220,19 @@ public class MainActivity extends AppCompatActivity {
         protected List<NewsItem> doInBackground(Void... params) {
             try {
 
-
-                //Creating OKHTTP Client
                 OkHttpClient client = new OkHttpClient();
 
                 Request request = new Request.Builder()
                         .url("http://24tv.ua/rss/all.xml")
                         .build();
 
-                //Transport the request and wait for response
                 Response response = client.newCall(request).execute();
 
                 String result = response.body().string();
 
                 Serializer serializer = new Persister();
 
-                newsResponse = serializer.read(NewsResponse.class, result.toString());
-
-
+                newsResponse = serializer.read(NewsResponse.class, result);
 
                 Log.v("newsResponse", newsResponse.chann.getNewsItem().toString());
                 return newsResponse.getChann().getNewsItem();
@@ -181,35 +241,27 @@ public class MainActivity extends AppCompatActivity {
                 Log.v("newsResponse", e.getMessage());
                 return null;
             }
-
         }
 
         @Override
         public void onPostExecute(List<NewsItem> list) {
             if (list != null) {
                 newsItemList = list;
-                RecyclerViewAdapter adapter = new RecyclerViewAdapter(list, getApplicationContext());
+
+                RecyclerViewAdapter adapter = new RecyclerViewAdapter(newsItemList, getApplicationContext());
                 recyclerView.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
                 swipeRefreshLayout.setRefreshing(false);
+                newsLoaded = true;
             }
         }
-
     }
 
     public boolean isOnline() {
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo nInfo = connectivityManager.getActiveNetworkInfo();
-        if (nInfo != null && nInfo.isConnected()) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
+        return nInfo != null && nInfo.isConnected();
     }
-
-
 
 
 }
